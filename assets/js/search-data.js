bb7c52e2a7577ac25f5e34182f6cc589deb5ed1b
xuy13023 <xuy13023@gmail.com>
2025-09-07 00:21:31 +0000

    Deploying to gh-pages from @ xuy13023/xuy13023.github.io@2c3ab42e5182134ce6b62bf1410e40666bdaf8a8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -451,9 +451,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-",
+          title: '',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
