3e2277f875b982bcb8aa9c3381201f8d33a48ab1
xuy13023 <xuy13023@gmail.com>
2025-09-08 00:51:01 +0000

    Deploying to gh-pages from @ xuy13023/xuy13023.github.io@a500b8b98d58893cf7aaf9da98d43ca849752632 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -451,8 +451,8 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
+          section: "News",},{id: "projects-shadow-education-under-the-double-reducation-policy-panel-study",
+          title: 'Shadow Education Under the Double Reducation Policy -- Panel Study',
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
