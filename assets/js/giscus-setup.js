12fde603fc236ef0f07e4327786f33f0000cd117
yijiaxu712 <xuy13023@gmail.com>
2025-10-06 21:51:31 +0000

    Deploying to gh-pages from @ yijiaxu712/xuy13023.github.io@25c651b5ab9442e5d5ddc19195fea99f62942ab2 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "xuy13023/xuy13023.github.io",
+    "data-repo": "yijiaxu712/xuy13023.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
