adaaee99bbc6128cfc162f99635b404570cab756
yijiaxu712 <xuy13023@gmail.com>
2025-10-16 23:20:09 +0000

    Deploying to gh-pages from @ yijiaxu712/yijiaxu712.github.io@1a795ec946d3c01ff6e0d6a9399bbde8706b6c88 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -17,8 +17,8 @@ ninja.data = [{
             window.location.href = "/projects/";
           },
         },{id: "nav-cv",
-          title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          title: "CV",
+          description: "Academic CV of Yijia Xu — including education, research, and professional experience.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
